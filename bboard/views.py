a594d72c3c18b6c1b44e5c00dce332e900b65f02
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-04-21 09:36:10 +0300

    additional changes

--- bboard/views.py
+++ bboard/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
+from .models import Bb
 
 def index(request):
-    print(request)
-    return HttpResponse("Here will be list of bulletin")
-
-
-
+    s = 'Список объявлений \r\n\r\n\r\n'
+    for bb in Bb.objects.order_by('-published'):
+        s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')
