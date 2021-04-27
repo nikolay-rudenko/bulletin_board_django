2e0011915e5b75f688af3e065d68f1d04e1742dd
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-04-27 22:01:33 +0300

    additional changes

--- bboard/models.py
+++ bboard/models.py
@@ -21,11 +21,13 @@ class Bb(models.Model):
 class Rubric(models.Model):
     name = models.CharField(max_length=20, db_index=True)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name_plural = 'Categories'
         verbose_name = 'Category'
         ordering = ['name']
 
-    def __str__(self):
-        return self.name
+
 
