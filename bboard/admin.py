4e8ce6792fbd7e79424a43af88c0d71ee24c826b
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-04-27 22:06:17 +0300

    additional changes

--- bboard/admin.py
+++ bboard/admin.py
@@ -3,7 +3,7 @@ from .models import Bb, Rubric
 
 
 class BdAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'price', 'published')
+    list_display = ('title', 'content', 'price', 'published', 'rubric')
     list_display_links = ('title', 'content')
     search_fields = ('title', 'content',)
 
